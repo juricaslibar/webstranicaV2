7a0f61f496ee72dba865bb2c46555ac0a24a60f3
juricaslibar <58658003+juricaslibar@users.noreply.github.com>
2025-01-14 00:30:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(session({
     }),
     cookie: {
         sameSite: 'lax'
-        secure: process.env.NODE_ENV === 'production', // Secure cookies in production
+        secure: process.env.NODE_ENV == "production", // Secure cookies in production
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000 // 1 day
     }
